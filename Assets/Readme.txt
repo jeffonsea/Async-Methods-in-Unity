7a23ea3b8320986591bfa8c2ae1c5972e88fdf27
Jeff Onsea <jeffonsea@gmail.com>
2016-05-12 00:00:28 +0200

    Initial Commit
    Initial commit for Async Methods in Unity Tutorial.

---
+++ Assets/Readme.txt
@@ -0,0 +1,5 @@
+Hey guys you can find the complete tutorial of this project on : 
+
+http://jeffonsea.xyz/wb/2016/05/11/unity-async-tasks/
+
+feel free to use this approach in your games. Cheers!
---
+++ Assets/Scripts/AsyncCallController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using UnityEngine;
+using System.Threading;
+using System.Collections;
+using System.Runtime.Remoting.Messaging;
+using System.Diagnostics;
+
+public class AsyncCallController : MonoBehaviour
+{
+    //The delegate that will execute the BeginInvoke method.
+    private delegate string AsyncTaskDelegate();
+
+    void Start()
+    {
+        CreateAsyncTask();
+    }
+
+    void CreateAsyncTask()
+    {
+        AsyncTaskDelegate asyncDelegate = new AsyncTaskDelegate(AsyncMethodBeingCalledOnOtherThread);
+        asyncDelegate.BeginInvoke(new AsyncCallback(MethodCalledWhenAsyncMethodCompleted), null);
+    }
+
+    private string AsyncMethodBeingCalledOnOtherThread()
+    {
+        Stopwatch stopWatch = new Stopwatch();
+        stopWatch.Start();
+
+        UnityEngine.Debug.Log("Started calculating the heavyStuff...");
+        HeavyCalculation();
+        UnityEngine.Debug.Log("Done!");
+
+        stopWatch.Stop();
+        float executionDelta = stopWatch.Elapsed.Milliseconds;
+
+        return "The async method has completed in -> " + executionDelta + " milliseconds.";
+    }
+
+    private void HeavyCalculation()
+    {
+        UnityEngine.Debug.Log("Doing some heavy math calculations..");
+
+        //Just an example. Dont laugh me out LoLz..
+        float heavyStuffValue = 1 + 1;
+    }
+    
+    private void MethodCalledWhenAsyncMethodCompleted(IAsyncResult ar)
+    {
+        AsyncResult asyncResult = (AsyncResult)ar;
+        AsyncTaskDelegate asyncDelegate = (AsyncTaskDelegate)asyncResult.AsyncDelegate;
+        string returnData = asyncDelegate.EndInvoke(ar);
+
+        UnityEngine.Debug.Log("Data returned from async callback [ " + returnData + " ]");
+    }
+}
